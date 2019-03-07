88d0fe75b7cef61901847301d57e991d08b0d78b
Gerald Wong Han Feng <ramza88@gmail.com>
2019-03-07 11:39:36 +0800

    no message

--- work/program/scripts/0_install.bat
+++ work/program/scripts/0_install.bat
@@ -1,2 +1,8 @@
-cd..
-yarn add --modules-folder node_modules merge-img discord.js dotenv sprintf-js request request-promise-native app-module-path @types/sprintf-js @types/request-promise-native @types/node @types/core-js @types/dotenv jimp
+cd ..
+rmdir /S /Q node_modules
+del /Q package.json
+del /Q yarn.lock
+
+call yarn init -y
+call yarn add --modules-folder node_modules merge-img discord.js dotenv sprintf-js request request-promise-native app-module-path @types/sprintf-js @types/request-promise-native @types/node @types/core-js @types/dotenv jimp
+pause
