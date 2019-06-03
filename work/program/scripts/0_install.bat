8365c8fad5cb4995edb649c02d6147df7e7650b5
GERALD WONG <ramza88@gmail.com>
2019-06-03 21:26:16 +0800

    updated map.ts and packages

--- work/program/scripts/0_install.bat
+++ work/program/scripts/0_install.bat
@@ -2,7 +2,7 @@
 Pushd "%~dp0"
 
 
-set MODULES=merge-img@2.1.3 discord.js@11.4.2 sprintf-js@1.1.2 request@2.88.0 request-promise-native@1.0.7 app-module-path@2.2.0 @types/sprintf-js@1.1.2 @types/request-promise-native@1.0.15 @types/node@11.12.2 @types/core-js@2.5.0 jimp@0.6.1
+set MODULES=discord.js@11.5.1 request-promise-native@1.0.7 sprintf-js@1.1.2 request@2.88.0 app-module-path@2.2.0 jimp@0.6.4 @types/request-promise-native@1.0.16 @types/core-js@2.5.0 @types/node@12.0.4 @types/sprintf-js@1.1.2 merge-img@2.1.3  
 
 cd ..
 echo Uninstalling modules...
--- work/program/src/splatoon/Map.ts
+++ work/program/src/splatoon/Map.ts
@@ -5,29 +5,29 @@ export interface MapInfo {
 }
 
 const mapList: Map<string, MapInfo> = new Map<string, MapInfo>();
-mapList.set("reef", 		{filename: "reef.png", 			enName: "The Reef", 			jpName: "あ"});
-mapList.set("fitness", 		{filename: "fitness.png",		enName: "Musselforge Fitness", 	jpName: "あ"});
-mapList.set("mainstage",	{filename: "mainstage.png", 	enName: "Starfish MainStage", 	jpName: "あ"});
-mapList.set("humpback", 	{filename: "humpback.png", 		enName: "Humpback Pump Track", 	jpName: "あ"});
-mapList.set("academy", 		{filename: "academy.png", 		enName: "Inkblot Art Academy", 	jpName: "あ"});
-mapList.set("shipyard",		{filename: "shipyard.png", 		enName: "Sturgeon Shipyard", 	jpName: "あ"});
-mapList.set("towers", 		{filename: "tower.png", 		enName: "Moray Towers", 		jpName: "あ"});
-mapList.set("port", 		{filename: "port.png", 			enName: "Port Mackerel", 		jpName: "あ"});
-mapList.set("manta",		{filename: "manta.png", 		enName: "Manta Maria", 			jpName: "あ"});
-mapList.set("dome", 		{filename: "dome.png",			enName: "Kelp Dome", 			jpName: "あ"});
-mapList.set("canal", 		{filename: "canal.png", 		enName: "Snapper Canal", jpName: "あ"});
-mapList.set("skatepark",	{filename: "skatepark.png", 	enName: "Blackbelly Skatepark", jpName: "あ"});
-mapList.set("mart",			{filename: "mart.png", 			enName: "MakoMart", jpName: "あ"});
-mapList.set("pumptrack", 	{filename: "pumptrack.png", 	enName: "Walleye Warehouse", jpName: "あ"});
-mapList.set("institute", 	{filename: "institute.png", 	enName: "Shellendorf Institute", jpName: "あ"});
-mapList.set("mall", 		{filename: "mall.png", 			enName: "Arowana Mall", jpName: "あ"});
-mapList.set("arena", 		{filename: "arena.png",			enName: "Goby Arena", jpName: "あ"});
-mapList.set("pit", 			{filename: "pit.png", 			enName: "Piranha Pit", jpName: "あ"});
-mapList.set("camp", 		{filename: "camp.png",	 		enName: "Camp Triggerfish", jpName: "あ"});
-mapList.set("world", 		{filename: "world.png",			enName: "Wahoo World", jpName: "あ"});
-mapList.set("hotel", 		{filename: "hotel.png", 		enName: "New Albacore Hotel", jpName: "あ"});
-mapList.set("games", 		{filename: "game.png", 			enName: "Ancho-V Games", jpName: "あ"});
-mapList.set("pavilion", 	{filename: "pavilion.png",		enName: "Skipper Pavilion", jpName: "あ"});
+mapList.set("reef", 		{filename: "reef.png", 			enName: "The Reef", 				jpName: "バッテラストリート"});
+mapList.set("fitness", 		{filename: "fitness.png",		enName: "Musselforge Fitness", 		jpName: "フジツボスポーツクラブ"});
+mapList.set("mainstage",	{filename: "mainstage.png", 	enName: "Starfish MainStage", 		jpName: "ガンガゼ野外音楽堂"});
+mapList.set("humpback", 	{filename: "humpback.png", 		enName: "Humpback Pump Track", 		jpName: "コンブトラック"});
+mapList.set("academy", 		{filename: "academy.png", 		enName: "Inkblot Art Academy", 		jpName: "海女美術大学"});
+mapList.set("shipyard",		{filename: "shipyard.png", 		enName: "Sturgeon Shipyard", 		jpName: "チョウザメ造船"});
+mapList.set("towers", 		{filename: "tower.png", 		enName: "Moray Towers", 			jpName: "タチウオパーキング"});
+mapList.set("port", 		{filename: "port.png", 			enName: "Port Mackerel", 			jpName: "ホッケふ頭"});
+mapList.set("manta",		{filename: "manta.png", 		enName: "Manta Maria", 				jpName: "マンタマリア号"});
+mapList.set("dome", 		{filename: "dome.png",			enName: "Kelp Dome", 				jpName: "モズク農園"});
+mapList.set("canal", 		{filename: "canal.png", 		enName: "Snapper Canal", 			jpName: "エンガワ河川敷"});
+mapList.set("skatepark",	{filename: "skatepark.png", 	enName: "Blackbelly Skatepark", 	jpName: "Bバスパーク"});
+mapList.set("mart",			{filename: "mart.png", 			enName: "MakoMart", 				jpName: "ザトウマーケット"});
+mapList.set("pumptrack", 	{filename: "pumptrack.png", 	enName: "Walleye Warehouse", 		jpName: "ハコフグ倉庫"});
+mapList.set("institute", 	{filename: "institute.png", 	enName: "Shellendorf Institute", 	jpName: "デボン海洋博物館"});
+mapList.set("mall", 		{filename: "mall.png", 			enName: "Arowana Mall", 			jpName: "アロワナモール"});
+mapList.set("arena", 		{filename: "arena.png",			enName: "Goby Arena", 				jpName: "アジフライスタジアム"});
+mapList.set("pit", 			{filename: "pit.png", 			enName: "Piranha Pit", 				jpName: "ショッツル鉱山"});
+mapList.set("camp", 		{filename: "camp.png",	 		enName: "Camp Triggerfish", 		jpName: "モンガラキャンプ場"});
+mapList.set("world", 		{filename: "world.png",			enName: "Wahoo World", 				jpName: "スメーシーワールド"});
+mapList.set("hotel", 		{filename: "hotel.png", 		enName: "New Albacore Hotel", 		jpName: "ホテルニューオートロ"});
+mapList.set("games", 		{filename: "game.png", 			enName: "Ancho-V Games", 			jpName: "アンチョビットゲームズ"});
+mapList.set("pavilion", 	{filename: "pavilion.png",		enName: "Skipper Pavilion", 		jpName: "ムツゴ楼"});
 
 export function getMap(key: string): MapInfo | undefined {
     return mapList.get(key);
