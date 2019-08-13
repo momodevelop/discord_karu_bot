932c034fc3aedf928d1727f631a765bb808e3147
GERALD WONG <ramza88@gmail.com>
2019-08-14 00:38:10 +0800

    Create Missyou.ts

---
+++ work/program/src/responses/res/Missyou.ts
@@ -0,0 +1,20 @@
+﻿import { hasWords } from 'common/Utils';
+import { ResponseBase } from 'libs/Responder/Responder';
+import { CallbackParams } from '../CallbackParams'
+
+class cResponse implements ResponseBase<CallbackParams> {
+
+	public async exec(params: CallbackParams): Promise<boolean> {
+		let f = hasWords;
+		let c = params.msg.content;
+		if (hasWords(c, ["miss you", "missed you"])) {
+			await params.msg.channel.send("Aww I miss you too! :kissing_smiling_eyes:");
+			return true;
+		}
+		return false;
+	}
+}
+
+export default function () {
+	return new cResponse();
+}
